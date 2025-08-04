7334605ad6ef34f5af40de58e685e9a85704c235
Cainan-Kai <cainanmendes8@gmail.com>
2025-08-04 14:48:03 -0300

    Add files via upload

---
+++ Html e Css/index.js
@@ -0,0 +1,11 @@
+const madrinha = {
+  nome: "bhe",
+  filha: {
+    nome: "Tia Ana",
+    papel: "irmã por parte de madrinha",
+    filha: {
+      nome: "Julia",
+      papel: "prima"
+    }
+  }
+};
